b8dc09dbf712ea54f224e4d3bf5aae556e506938
Abel-Git2103 <abel_jornet@hotmail.com>
2022-02-21 14:17:15 +0100

    INSERTAR PROYECTO

---
+++ src/main/java/com/example/Gradle/Ta23Ud232Application.java
@@ -0,0 +1,13 @@
+package com.example.Gradle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ta23Ud232Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ta23Ud232Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/Gradle/Ta23Ud232ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Gradle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ta23Ud232ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
